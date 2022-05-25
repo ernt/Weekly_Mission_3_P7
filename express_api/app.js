8d15617fecd89e54ee4b4aad2ce5e2452fbacffc
Ernt <ernestn@ciencias.unam.mx>
2022-05-25 00:36:53 -0500

    P7 final

---
+++ express_api/app.js
@@ -0,0 +1,61 @@
+// Usando objeto express
+const express = require('express')
+// App de Express
+const app = express()
+app.use(express.json()) // Indicamos que usaremos JSON
+// Puerto en que vamos a ver nuestra app: localhost:3000
+const port = 3000
+
+// Con esto inicializamos esta app
+app.listen(port, () => {
+ console.log(`Example app listening on port ${port}`)
+})
+
+
+app.get('/v1/explorers',(req,res)=> {
+
+    console.log (`Api Explorers Get All request ${new Date}`)
+    const explorer1 = {id:1,name:"Ernt1"}
+    const explorer2 = {id:2,name:"Ernt2"}
+    const explorer3 = {id:3,name:"Ernt3"}
+    const explorer4 = {id:4,name:"Ernt4"}
+    const explorers = [explorer1,explorer2,explorer3,explorer4]
+    res.status(200).json(explorers)
+})
+
+app.get('/v1/explorers/:id',(req,res)=> {
+
+    console.log (`Api Explorers Get  request ${new Date}`)
+    console.log (`Getting explorer with id  ${req.params.id}`)
+    const explorer = {id:1,name:"Ernt1"}
+   
+    res.status(200).json(explorer)
+})
+
+app.post('/v1/explorers',(req,res)=> {
+
+    console.log (`Api Explorers POST  request ${new Date}`)
+    
+    const requestBody = req.body
+   
+    res.status(201).json({message:"Created!:)"})
+})
+
+app.put('/v1/explorers/:id',(req,res)=> {
+
+    console.log (`Api Explorers POT  request ${new Date}`)
+    console.log (`Update explorer with id  ${req.params.id}`)
+    const requestBody = req.body
+   
+    res.status(200).json({message:"Update! :)"})
+})
+
+
+app.delete('/v1/explorers/:id',(req,res)=> {
+
+    console.log (`Api Explorers DELETE  request ${new Date}`)
+    console.log (`Delete explorer with id  ${req.params.id}`)
+    const requestBody = req.body
+   
+    res.status(200).json({message:"Deleted :O"})
+})
